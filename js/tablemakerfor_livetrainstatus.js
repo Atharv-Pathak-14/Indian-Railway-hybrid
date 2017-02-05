ba93cf02279b413362e0fc972b053627c67f1f43
rishabhnayak <rishabhnayak76@gmail.com>
2017-02-05 23:24:07 +0530

    new changes

--- js/tablemakerfor_livetrainstatus.js
+++ js/tablemakerfor_livetrainstatus.js
@@ -4,7 +4,7 @@
 function tablemakerforlivestatus(body,train_no,train_name) {
 
 
-    var livetabledata = "<table> <thead> <tr> <th>#</th><th>Station</th> <th>Sch. Arrival</th> <th>Sch. Departure</th><th>Delay</th></tr> </thead> </tbody>";
+    var livetabledata = "<table style='width:100%'> <thead> <tr> <th>#</th><th>Station</th> <th>Sch. Arrival</th> <th>Sch. Departure</th><th>Delay</th></tr> </thead> </tbody>";
 
     if (body.current_station.latemin > 0) {
         var trainposition = "<h4 strong><span  id=\"trainname\" style=\"color:midnightblue\"></span></h4><h5>Current Position :</strong><p style=\"color:red\">" + body.position + "</p></h5>";
@@ -16,7 +16,7 @@ function tablemakerforlivestatus(body,train_no,train_name) {
     var k=0;
     body.route.forEach(function(route) {
         if(k==route.day) {
-            livetabledata +="<tr><td></td><td style='color:navy;'><h5><strong>Day "+ (k+1)+"</strong></h5></td><td></td><td></td><td></td></tr>";
+            livetabledata +="<tr><td></td><td style='color:rgb(66,44,100)'><h5><strong>Day "+ (k+1)+"</strong></h5></td><td></td><td></td><td></td></tr>";
             k++;
         }
         if (route.no < body.current_station.no) {
@@ -43,10 +43,10 @@ function tablemakerforlivestatus(body,train_no,train_name) {
         } else {
             if (route.latemin <= 0) {
                 livetabledata += "<tr id='" + route.no + "'><td>" + route.no + ".</td><td> " + route.station_.name + "</td><td>" + route.scharr + "<br><pp style=\"color:green\">" + route.actarr + " </pp></td><td>" +
-                    route.schdep + "<br><pp style=\"color:green\">" + route.actdep + "</pp></td><td><pp style=\"color:green\">" + route.latemin + " min</pp></td></tr>";
+                    route.schdep + "<br><pp style=\"color:rgba(43,193,25,.55)\">" + route.actdep + "</pp></td><td><pp style=\"color:green\">" + route.latemin + " min</pp></td></tr>";
             } else {
                 livetabledata += "<tr id='" + route.no + "'><td>" + route.no + ".</td><td> " + route.station_.name + "</td><td>" + route.scharr + "<br><pp style=\"color:red\">" + route.actarr + " </pp></td><td>" +
-                    route.schdep + "<br><pp style=\"color:red\">" + route.actdep + "</pp></td><td><pp style=\"color:red\">" + route.latemin + " min</pp></td></tr>";
+                    route.schdep + "<br><pp style=\"color:rgba(43,193,25,.55)\">" + route.actdep + "</pp></td><td><pp style=\"color:red\">" + route.latemin + " min</pp></td></tr>";
             }
         }
 
